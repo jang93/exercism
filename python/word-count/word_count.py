be0b23f5a774b4a7390f4008de377cc01304088c
jang93 <jang93@gmail.com>
2020-11-25 09:41:37 +0800

    Simplified word-count regex

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -3,9 +3,8 @@ from collections import Counter
 
 def count_words(sentence):
     pattern = re.compile(r'''
-    (\d+ |                    # numbered word
-    [a-z]+'[a-z]+ |             # apostrophed word (match before lettered word as lettered word is a subset)
-    [a-z]+)                   # lettered word
+    (\d+ |                           # numbered word
+    [a-z]+(?:'[a-z]+)?)              # apostrophed word (match before lettered word as lettered word is a subset)
     ''', re.VERBOSE | re.I)
     matches = pattern.findall(sentence)
     return Counter([match.lower() for match in matches])
