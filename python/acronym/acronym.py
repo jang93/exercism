ad40f30f2a1920cb9169fb3316eddc80b6777677
jang93 <jang93@gmail.com>
2021-02-08 15:09:15 +0800

    Updated acronym.py - removed _ and replaced '-' with ' '

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -2,6 +2,7 @@ import re
 
 
 def abbreviate(words):
+    words = words.replace('_', '').replace('-', ' ')
     matches = re.findall(r'[A-Z\']+', words.upper())
     return ''.join([match[0] for match in matches])
 
